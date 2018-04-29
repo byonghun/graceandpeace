47b191e16e84a2770d3b34a4209ad60371285210
kwakiekwak <john.bh.kwak@gmail.com>
2018-04-28 18:37:41 -0700

    Set up React js

---
+++ resources/react/containers/App.jsx
@@ -0,0 +1,8 @@
+export default class extends Component {
+  render() {
+
+    return (
+      <p>Hello World YEEES</p>
+    )
+  }
+}
---
+++ resources/react/webpack/webpack.dev.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var path = require('path');
+var parentDir = path.join(__dirname, '../');
+
+module.exports = {
+  entry: [
+    path.join(parentDir, 'index.jsx')
+  ],
+  output: {
+    path: parentDir + '/build',
+    filename: 'app.js',
+    publicPath: 'http://localhost:8080/build'
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }, {
+      test: /\.less$/,
+      loaders: ["style-loader", "css-loader", "less-loader"]
+    }]
+  },
+  devServer: {
+    contentBase: parentDir,
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: ['.graphql', '.gql', '.web.js', '.js', '.jsx', '.css']
+  }
+};
