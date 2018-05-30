b49b7ca976e556531a7f0a34ea3a7d98ed7ef2ba
byonghun <john.bh.kwak@gmail.com>
2018-05-29 21:50:51 -0700

    Update files

--- project.clj
+++ project.clj
@@ -7,5 +7,6 @@
                  [http-kit "2.2.0"]
                  [ring "1.6.3"]
                  [selmer "1.11.0"]
+                 [com.zenboxapp/taika "0.1.4"]
                  [compojure "1.6.0"]]
   :main handler)
